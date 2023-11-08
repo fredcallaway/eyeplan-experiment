62d1afbe592c0bd4afd6ecbb90ec5b549775ab3e
fredcallaway <fredc@llaway.com>
2023-11-08 11:56:37 -0500

    process edf files

--- process_data.py
+++ process_data.py
@@ -1,20 +1,37 @@
 import os
 import json
 import pandas as pd
-
+import subprocess
 version = sys.argv[1]
 # wid = 'fred'
 
+
 trials = []
 for file in os.listdir(f"data/exp/{version}/"):
+    wid = file.replace('.json', '')
+    # wid = uid.rsplit('-', 1)[1]
+
+    # experimental data
     with open(f"data/exp/{version}/{file}") as f:
         data = json.load(f)
-        wid = file.rsplit('-', 1)[1].replace('.json', '')
-        for i, t in enumerate(data["trial_data"]):
-            t["wid"] = wid
-            t["trial_index"] = i
-            trials.append(t)
+    for i, t in enumerate(data["trial_data"]):
+        t["wid"] = wid
+        t["trial_index"] = i
+        trials.append(t)
+
+    # eyelink data
+    edf = f'data/eyelink/{wid}/raw.edf'
+    assert os.path.isfile(edf)
+    dest = f'data/eyelink/{wid}/samples.asc'
+    if os.path.isfile(edf) and not os.path.isfile(dest):
+        cmd = f'edf2asc {edf} {dest}'
+        output = subprocess.getoutput(cmd)
+        if 'Converted successfully' not in output:
+            print(f'Error parsing {edf}', '-'*80, output, '-'*80, sep='\n')
+
 
 os.makedirs(f'data/processed/{version}/', exist_ok=True)
 with open(f'data/processed/{version}/trials.json', 'w') as f:
     json.dump(trials, f)
+
+
