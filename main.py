213f415031397ca54f2c7f1baf186fd148751c0e
fredcallaway <fredc@llaway.com>
2023-11-28 07:15:28 -0500

    Revert "try downscaling"
    This reverts commit 950a54949ac9c379262f43d29bf79afba1bb8a6c.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main(config_number=None, name=None, test=False, fast=False, full=False):
     if test:
         # exp.parameters['summarize_every'] = 2
         # exp.intro()
+        # exp.practice(1)
         # exp.practice_timelimit()
         exp.setup_eyetracker()
         exp.show_gaze_demo()
