ecf95b03771bc10c7973060ce40edf4c22b8ccfa
fredcallaway <fredc@llaway.com>
2023-12-01 16:08:16 -0500

    main

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main(config_number=None, name=None, test=False, fast=False, full=False, hotf
         # exp.practice_timelimit()
         exp.setup_eyetracker()
         # exp.show_gaze_demo()
-        exp.intro_gaze()
-        exp.calibrate_gaze_tolerance()
+        # exp.intro_gaze()
+        # exp.calibrate_gaze_tolerance()
         exp.intro_contingent()
         exp.parameters['summarize_every'] = 2
         exp.run_main(10)
