917b105d4d03a4f6eb59d3f956a97dfd6464e27e
fredcallaway <fredc@llaway.com>
2024-04-10 10:26:07 -0400

    adjust params

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main(config_number=None, name=None, test=False, fast=False, full=False, mous
     if fast:
         kws['score_limit'] = 10
 
-    kws['force_rate'] = .3
-    kws['score_limit'] = 250
+    kws['force_rate'] = .2
+    kws['score_limit'] = 200
 
     exp = Experiment(config_number, name, full_screen=(not test) or full, test_mode=test, **kws)
     if test:
