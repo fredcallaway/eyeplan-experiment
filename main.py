5a2caf036fbd45a4de8aaa4f8b78d790178fa7de
fredcallaway <fredc@llaway.com>
2024-06-27 21:45:51 -0400

    test forced

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ def main(config_number=None, name=None, test=False, fast=False, full=False, mous
 
     exp = Experiment(config_number, name, full_screen=(not test) or full, test_mode=bool(test), **kws)
     if test == 'main':
+        # exp.run_main()
         exp.debug_main()
         return
+    elif test == 'forced':
+        exp.intro_forced()
+        exp.practice_forced(10)
     else:
         try:
             exp.intro()
