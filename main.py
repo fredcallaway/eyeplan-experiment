7479fb2b4459b07d0afb8dabb0a0a84999905931
fredcallaway <fredc@llaway.com>
2024-09-11 11:36:15 -0400

    testing eye tracking quality

--- main.py
+++ main.py
@@ -2,74 +2,27 @@ from experiment import Experiment
 from fire import Fire
 import logging
 
-def main(config_number=None, name=None, test=False, fast=False, full=False, mouse=False, hotfix=False, **kws):
+def main(config_number=None, name='test', test=False, fast=False, full=False, mouse=False, hotfix=False, **kws):
     if test and name is None:
         name = 'test'
     if fast:
         kws['score_limit'] = 10
-    exp = Experiment(config_number, name, full_screen=(not test) or full, **kws)
-    if test:
-        if test == 'survey':
-            exp.save_data(survey=True)
-        elif test == 'main':
-            exp.run_main()
-        else:
-            # exp.intro()
-            # exp.practice_start()
-            exp.practice(2)
-            # exp.practice_timelimit()
-            exp.setup_eyetracker(mouse)
-            exp.show_gaze_demo()
-            exp.intro_gaze()
-            exp.calibrate_gaze_tolerance()
-            exp.intro_contingent()
-            exp.intro_main()
-            exp.run_main()
-            # exp.do_survey()
-            # exp.save_data()
-        return
-    else:
+    exp = Experiment(config_number, name, full_screen=(not test) or full, skip_survey = True, **kws)
+
+    try:
+        exp.setup_eyetracker(mouse)
+        exp.show_gaze_demo()
+        exp.calibrate_gaze_tolerance()
+        exp.intro_contingent()
+        exp.intro_main()
+        exp.run_main()
+    except:
+        logging.exception('Exception in main, saving data...')
         try:
-            if fast:
-                exp.intro()
-                exp.practice_start()
-                exp.practice(1)
-                exp.setup_eyetracker(mouse)
-                exp.show_gaze_demo()
-                exp.intro_gaze()
-                exp.calibrate_gaze_tolerance()
-                exp.intro_contingent()
-                exp.intro_main()
-                exp.run_main()
-                # exp.do_survey()
-            else:
-                exp.intro()
-                exp.practice_start()
-                exp.practice(2)
-                exp.setup_eyetracker(mouse)
-                exp.show_gaze_demo()
-                exp.intro_gaze()
-                exp.calibrate_gaze_tolerance()
-                exp.intro_contingent()
-                exp.intro_main()
-                exp.run_main()
-
-            exp.save_data(survey=True)
+            exp.save_data()
         except:
-            if test:
-                exit(1)
-            logging.exception('Uncaught exception in main')
-            exp.win.clearAutoDraw()
-            exp.win.showMessage("Drat! The experiment has encountered an error.\nPlease inform the experimenter.")
-            exp.win.flip()
-            try:
-                exp.save_data()
-                raise
-            except:
-                logging.exception('error on second save data attempt')
-                exp.emergency_save_data()
-                raise
-
+            logging.exception('Failed to save data, trying again.')
+            exp.emergency_save_data()
 
 if __name__ == '__main__':
     Fire(main)
